630cd2edb324cae7977c709fe08ae443c2cb6342
gabyballester <gaby.ballester@gmail.com>
2020-08-17 19:48:44 +0200

    EditingNewProjectComponent: developet the state management of the component

--- src/components/projects/NewProject.jsx
+++ src/components/projects/NewProject.jsx
@@ -1,18 +1,51 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const NewProject = () => {
+  //state para new project
+  const [newProject, saveNewProject] = useState({
+    id: "", // esto lo generaremos con una librería posteriormente
+    nombre: "",
+  });
+
+  //Extraigo propiedades del proyecto con destructuring
+  const { id, nombre } = newProject;
+
+  // Función lee contenido del input y guarda estado si objeto cambia
+  const onChangeProject = (e) => {
+    saveNewProject({
+      ...NewProject,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // Cuando usuario envía formulario de nuevo proyecto
+  const onSubmitProject = (e) =>{
+      e.preventDefault();
+
+      //validar el input
+
+      // agregar al state
+
+      //reiniciar el form
+
+  }
+
   return (
     <Fragment>
       <button type="button" className="btn btn-block btn-primario">
         Nuevo Proyecto
       </button>
-      <form className="formulario-nuevo-proyecto">
+
+      <form onSubmit={onSubmitProject} className="formulario-nuevo-proyecto">
         <input
           type="text"
           className="input-text"
           placeholder="Nombre Proyecto"
           name="nombre"
+          value={nombre}
+          onChange={onChangeProject}
         />
+
         <input
           type="submit"
           className="btn btn-primario btn-block"
