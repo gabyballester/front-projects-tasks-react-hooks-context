d9e044860e5ce42a47490fdc1f1e988deb2d25c2
gabyballester <gaby.ballester@gmail.com>
2020-08-17 19:59:41 +0200

    CreatingProjectsListComponent: Created ProjectsList.jsx & Project.jsx which will be imported and inyected on ProjectsList.jsx

---
+++ src/components/projects/Project.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Project = () => {
+  return <p>Proyecto</p>;
+};
+
+export default Project;
---
+++ src/components/projects/ProjectsList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Project from "./Project";
+
+const ProjectsList = () => {
+  return (
+    <ul className="listado-proyectos">
+      <Project />
+    </ul>
+  );
+};
+
+export default ProjectsList;
