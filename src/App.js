51cd19d9f083920380dc0a8412aa8d04c543611b
gabyballester <gaby.ballester@gmail.com>
2020-08-17 14:02:12 +0200

    CreatingComponentsAndRouting: App.js set up with routing and routes to login, new account and projects

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route  } from 'react-router-dom';
 
 function App() {
   return (
-    <h1>MERNTasks</h1>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login}/>
+        <Route exact path="/nueva-cuenta" component={NewAccount}/>
+        <Route exact path="/proyectos" component={Projects}/>
+      </Switch>
+    </Router>
   );
 }
 
