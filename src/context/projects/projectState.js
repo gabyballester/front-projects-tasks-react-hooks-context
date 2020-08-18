7e78b77d69bc8fe87e919f13bf90a04a85fa19d2
gabyballester <gaby.ballester@gmail.com>
2020-08-18 16:23:57 +0200

    CreatingProjectsProvider: Set up the projectState.js

---
+++ src/context/projects/projectState.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import projectContext from './projectContext';
+
+// State inicial del proyecto
+
+const projectContext = props => {
+    const initialState = {
+        //empiezo con nuevo proyecto
+        newProject: false
+    }
+}
