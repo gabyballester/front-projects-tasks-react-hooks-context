e07fceda8d1b915da58e66434d3f0fad53e46dad
gabyballester <gaby.ballester@gmail.com>
2020-08-18 19:34:01 +0200

    ManagingNewProjectState: Centralicing the projects inside the projectState and showing them up using useContext in the ProjectsList.jsx

--- src/components/projects/ProjectsList.jsx
+++ src/components/projects/ProjectsList.jsx
@@ -1,19 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import Project from "./Project";
+import projectContext from "../../context/projects/projectContext";
 
 const ProjectsList = () => {
-
-    const projects = [
-        {name: 'Tienda virtual'},
-        {name: 'Intranet'},
-        {name: 'Diseño UX/UI'},
-    ]
-
+  // Extraer proyectos de state inicial
+  const projectsContext = useContext(projectContext);
+  // destructuro proyectos del context
+  const { projects } = projectsContext;
+  // compruebo que hay projects de no ser así, devuelve null
+  if(projects.length===0) return null;
+// renderizado del componente
   return (
     <ul className="listado-proyectos">
-        {projects.map(project=>(
-            <Project project={project}/>
-        ))}
+      {projects.map((project) => (
+        <Project key={project.id} project={project} />
+      ))}
     </ul>
   );
 };
--- src/context/projects/projectState.js
+++ src/context/projects/projectState.js
@@ -7,6 +7,12 @@ import { PROJECT_FORM } from '../../types/index';
 // La regla de hooks dice que ha de ser en mayúscula
 const ProjectState = props => {
     const initialState = {
+        projects: [
+            { id: 1, name: 'Tienda virtual' },
+            { id: 2, name: 'Intranet' },
+            { id: 3, name: 'Diseño UX/UI' },
+            {id: 4, name: 'MERN'}
+        ],
         //empiezo con nuevo proyecto
         form: false
     }
@@ -27,8 +33,14 @@ const ProjectState = props => {
     return (
         <projectContext.Provider
             value={{
+
+                //state props
+                projects: state.projects,
                 form: state.form, //state en minusculas
+
+                //functions
                 showForm // función en 2 palabras y mayúscula la 2ª
+
             }}
         >
             {props.children}
